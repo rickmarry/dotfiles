056d5dbc7039e607503c8d24f0a03d527c4c334e
Rick Marry <rick.marry@gmail.com>
2019-02-14 16:32:43 -0500

    add scripts dir with xrandr settings as my first one

--- scripts/setModesForXrandr.sh
+++ scripts/setModesForXrandr.sh
@@ -1,3 +1,3 @@
-xrandr  --output DP2 --rotate left --brightness 0.6 --left-of DP1
+xrandr  --output DP2 --rotate left --brightness 0.5 --left-of DP1
 xrandr  --output HDMI3 --rotate left --right-of DP1
 
